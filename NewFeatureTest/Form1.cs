45cd66df54d4d10e7082385609dd7f3129e21f30
NeNe <nene@jloli.cc>
2020-10-05 11:21:06 +0800

    首次提交

---
+++ NewFeatureTest/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace NewFeatureTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            int xxx = 2313;
+
+            string s1 = "abc";
+            string s2 = "abc";
+
+            if (xxx > 5)
+                Console.Write("dwadwadwaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaawad");
+        }
+    }
+}
